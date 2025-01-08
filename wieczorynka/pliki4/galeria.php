eb81cb934a87a5246215d05de8207fd2e876ffb2
Adam Karczewski <43960797+adamkar26@users.noreply.github.com>
2025-01-08 20:02:29 +0100

    Add files via upload

---
+++ wieczorynka/pliki4/galeria.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Zdjęcia</h1>
+
+    </header>
+    <main>
+        <section class="left">
+            <h2>Tematy zdjęć</h2>
+            <ol>
+                <li>Zwierzęta</li>
+                <li>Krajobrazy</li>
+                <li>Miasta</li>
+                <li>Przyroda</li>
+                <li>Samochody</li>
+            </ol>
+        </section>
+
+        <section class="center">
+            <section class="photo">
+                <img src="cat.jpg" alt="">
+                <h3>Nagłowek</h3>
+                <p>Paragraf</p>
+                <a href="">Link</a>
+            </section>
+
+            <section class="photo">
+                <img src="cat.jpg" alt="">
+                <h3>Nagłowek</h3>
+                <p>Paragraf</p>
+                <a href="">Link</a>
+            </section>
+
+            <section class="photo">
+                <img src="cat.jpg" alt="">
+                <h3>Nagłowek</h3>
+                <p>Paragraf</p>
+                <a href="">Link</a>
+            </section>
+
+        </section>
+
+        <section class="right">
+            <h2>Najbardziej lubiane</h2>
+            <strong>Zobacz wszystkie nasze zdjęcia</strong>
+
+        </section>
+    </main>
+
+    <footer>
+        <h5>Stronę wykonał: Adam Karczewski</h5>
+    </footer>
+
+    
+</body>
+</html>
